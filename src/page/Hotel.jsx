e67c80259abceb0ac9a63d89041486651c47a3e1
Avanin <asamuvictor03@gmail.com>
2023-07-30 00:17:17 +0100

    started header

--- src/page/Hotel.jsx
+++ src/page/Hotel.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import "../styles/HotelStyle.css"
 const Hotel = () => {
   return (
     <>
-     
+      <main>
+        <section className='bodyStyle'>
+         
+        </section>
+      </main>
     </>
   )
 }
