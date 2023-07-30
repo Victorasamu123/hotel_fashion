875d0f566af0e46565d53ca780a8db4c815fc196
Avanin <asamuvictor03@gmail.com>
2023-07-30 02:10:17 +0100

    completed header component

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
-
+import "../styles/HeaderStyler.css"
+import logo from "../assets/logo_fashion.svg"
+import flag from '../assets/circle-flags_uk.svg'
+import profile from "../assets/ri_account-circle-line.svg"
+import arrowDown from "../assets/arrow_down.svg"
 const Header = () => {
   return (
-    <div>Header</div>
+    <section className='headerContainer'>
+        <div className='logoContainer'>
+        <img src={logo} alt="" />
+        </div>
+        <div>
+          <ul className='listContainer'>
+            <li className='listItems'>Hotel</li>
+            <li className='listItems'>Brand</li>
+            <li className='listItems'>About Us</li>
+            <li className='listItemsTwo'>How It Works <img src={arrowDown} alt="" /></li>
+            <li className='listItems'>Pricing</li>
+            <li className='listItems'>Contact</li>
+          </ul>
+        </div>
+        <div className='imgContainer'>
+         <img src={flag} alt="" />
+         <img src={profile} alt="" />
+        </div>
+    </section>
   )
 }
 
--- src/page/Hotel.jsx
+++ src/page/Hotel.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "../styles/HotelStyle.css"
+import Header from '../component/Header';
 const Hotel = () => {
   return (
     <>
       <main>
         <section className='bodyStyle'>
-         
+         <Header/>
+         <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique ullam perferendis voluptatem nihil accusantium cumque assumenda mollitia, itaque sint autem fuga ducimus ipsum rem quibusdam rerum magni consequuntur natus? Provident accusamus facilis maxime, ad, unde adipisci expedita amet eveniet dolore pariatur cupiditate maiores esse asperiores sunt modi minima consectetur, quaerat commodi consequuntur. Blanditiis ipsam rerum velit quo earum reprehenderit unde error sunt odit! Commodi iste sed sit exercitationem similique ipsa odio incidunt consequuntur dolores, omnis adipisci, non necessitatibus excepturi hic, deserunt accusantium magnam ipsum. A illo blanditiis at quidem laudantium nisi porro sed numquam, dolores, temporibus mollitia, sit laboriosam repudiandae!</div>
         </section>
       </main>
     </>
