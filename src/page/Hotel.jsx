9161b8cbb72f8f75f54421d195ed741e135c7fe8
Avanin <asamuvictor03@gmail.com>
2023-07-30 02:24:34 +0100

    started hero section

--- src/page/Hotel.jsx
+++ src/page/Hotel.jsx
@@ -7,7 +7,9 @@ const Hotel = () => {
       <main>
         <section className='bodyStyle'>
          <Header/>
-         <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique ullam perferendis voluptatem nihil accusantium cumque assumenda mollitia, itaque sint autem fuga ducimus ipsum rem quibusdam rerum magni consequuntur natus? Provident accusamus facilis maxime, ad, unde adipisci expedita amet eveniet dolore pariatur cupiditate maiores esse asperiores sunt modi minima consectetur, quaerat commodi consequuntur. Blanditiis ipsam rerum velit quo earum reprehenderit unde error sunt odit! Commodi iste sed sit exercitationem similique ipsa odio incidunt consequuntur dolores, omnis adipisci, non necessitatibus excepturi hic, deserunt accusantium magnam ipsum. A illo blanditiis at quidem laudantium nisi porro sed numquam, dolores, temporibus mollitia, sit laboriosam repudiandae!</div>
+         <div className='heroSectionContainer'>
+           hello
+         </div>
         </section>
       </main>
     </>
